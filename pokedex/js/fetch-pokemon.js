53c8cc5a45b2ee78441240e7a3cb3d986ba0eded
Thiago da Silveira Antunes <182210060@senacinformatica.br>
2024-10-28 21:28:56 -0300

    js-Css-html-adicionei-as-cartas

--- pokedex/js/fetch-pokemon.js
+++ pokedex/js/fetch-pokemon.js
@@ -1,1 +1,48 @@
-/*Thiago*/ 
+async function BuscarPokemon(generation) {
+    const container = document.getElementById(`gen${generation}`);
+    container.innerHTML = ''; 
+
+    const inicia = generation === 1 ? 1 : geracaoLimite[generation - 1] + 1;
+    const termina = geracaoLimite[generation];
+
+ 
+    for (let i = inicia; i <= termina; i++) {
+        const chama = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+        const dados = await chama.json();
+        CriaPokemon(dados, container); 
+    }
+
+    container.style.display = 'flex'; 
+}
+
+function CriaPokemon(pokemon, container) {
+    const pokemonCard = document.createElement('div');
+    pokemonCard.classList.add('pokemon'); 
+
+    if (pokemonLen.includes(pokemon.id)) {
+        pokemonCard.classList.add('legendary'); 
+        pokemonCard.dataset.raridade = 'lendário';
+    } else if (pokemon.id % 3 === 0) {
+        pokemonCard.dataset.raridade = 'raro'; 
+    } else {
+        pokemonCard.dataset.raridade = 'comum';
+    }
+
+    const tipo = pokemon.types[0].type.name; 
+    pokemonCard.classList.add(`type-${tipo}`);
+
+    // Monta o HTML do card
+    const nome = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1); 
+    const types = pokemon.types.map(type => type.type.name).join(', '); 
+
+    pokemonCard.innerHTML = `
+        <h2>${nome}</h2>
+        <img src="${pokemon.sprites.front_default}" alt="${nome}">
+        <p class="type">Tipo: ${types}</p>
+        <p><strong>Altura:</strong> ${pokemon.height / 10} m</p>
+        <p><strong>Peso:</strong> ${pokemon.weight / 10} kg</p>
+    `;
+
+    pokemonCard.onclick = () => ExibirModal(pokemon);
+    container.appendChild(pokemonCard); 
+}
--- pokedex/js/modal.js
+++ pokedex/js/modal.js
@@ -1,1 +1,57 @@
-/*Thiago*/
+let indicePokemon = 0;
+
+function ExibirModal(pokemon) {
+    indicePokemon = pokemon.id;
+
+    const modal = document.getElementById('pokemonModal');
+    document.getElementById('modalNome').innerText = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    document.getElementById('modalImagem').src = pokemon.sprites.front_default;
+    document.getElementById('modalAltura').innerText = (pokemon.height / 10).toFixed(1);
+    document.getElementById('modalPeso').innerText = (pokemon.weight / 10).toFixed(1);
+    document.getElementById('modalTipo').innerText = 'Tipo: ' + pokemon.types.map(type => type.type.name).join(', ');
+    document.getElementById('modalHabilidade').innerText = pokemon.abilities.map(ability => ability.ability.name).join(', ');
+
+    const listar = document.getElementById('modalEstatistica');
+    listar.innerHTML = '';
+    pokemon.stats.forEach(stat => {
+        const listItem = document.createElement('li');
+        listItem.innerText = `${stat.stat.name}: ${stat.base_stat}`; 
+        listar.appendChild(listItem);
+    });
+
+   
+    const modalContent = document.querySelector('.modal-content');
+    modalContent.classList.add('girar');
+    
+   
+    setTimeout(() => {
+        modalContent.classList.remove('girar');
+    }, 500); 
+
+    modal.style.display = 'flex'; 
+    botaoNav(pokemon.id); 
+}
+
+function navegar(direction) {
+    const proxId = indicePokemon + direction; 
+    fetch(`https://pokeapi.co/api/v2/pokemon/${proxId}`)
+        .then(chama => {
+            if (chama.ok) {
+                return chama.json();
+            } else {
+                throw new Error('Pokémon não encontrado'); 
+            }
+        })
+        .then(dados => ExibirModal(dados)) 
+        .catch(error => console.error(error)); 
+}
+
+function botaoNav(currentId) {
+    document.getElementById('anteButton').style.display = currentId > 1 ? 'inline-block' : 'none'; 
+    document.getElementById('proxiButton').style.display = currentId < 1010 ? 'inline-block' : 'none'; 
+}
+
+function fechaModal() {
+    const modal = document.getElementById('pokemonModal');
+    modal.style.display = 'none'; 
+}
