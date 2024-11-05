211477c497cc2479ef1dd7206546659511546b1c
Thiago da Silveira Antunes <182210060@senacinformatica.br>
2024-11-05 20:42:31 -0300

    CartinhasResponsividade

--- pokedex/js/animacao.js
+++ pokedex/js/animacao.js
@@ -1,14 +1,23 @@
 const imagem = document.getElementById('imagem');
 const content = document.querySelector('.content');
-let posicao = content.clientWidth; 
+let posicao = content.clientWidth;  
+let sentido = -1; 
 
 function animarImagem() {
-    posicao -= 1.5; 
+    posicao += 1.7 * sentido; 
+
+   
+    if (posicao < -imagem.width || posicao > content.clientWidth) {
+        
+
+  
+        imagem.style.transition = 'transform 0.5s ease-in-out';
+        imagem.style.transform = `rotateY(${sentido === 1 ? '180deg' : '0deg'})`;
+    }
+
+   
     imagem.style.left = posicao + 'px';
 
-    if (posicao < -imagem.width) {
-        posicao = content.clientWidth; 
-    }
 
     requestAnimationFrame(animarImagem);
 }
